2b8dd3af4965b8542226649356db575617f7e2e3
Jatin Kathuria <jtn.kathuria@gmail.com>
2019-12-23 15:50:45 +0530

    Initial Commit

---
+++ template/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: `./tsconfig.json`
+  },
+};
